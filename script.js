061227a2764f42fdf32d9c7f54d9b05dd6c41a64
Vandu-Shre <vand.shree@gmail.com>
2022-08-23 22:08:57 +0530

    Refactoring

--- script.js
+++ script.js
@@ -1,32 +1,48 @@
 'use strict'
 
 let num = Math.floor(Math.random() * 20) + 1
+let score = 20
 
 const handleCheck = function () {
   const guess = Number(document.querySelector('.guess').value)
-  let score = Number(document.querySelector('.score').textContent)
+  const highscore = document.querySelector('.highscore').textContent
   let message
 
-  if (num - guess > 0) message = 'Wrong! Try going higher.'
-  if (num - guess < 0) message = 'Wrong! Try going lower.'
-  if (num - guess === 1 || num - guess === -1) message = 'You are very close'
-  if (!guess || guess < 1 || guess > 20) message = 'Enter a valid number'
-  console.log(num, guess)
-
-  if (num === guess) {
-    document.querySelector('.message').textContent = 'You got it!!'
-    if (score > document.querySelector('.highscore').textContent)
+  if (score === 0) {
+    message = 'You have exhausted your chances. Play again!'
+  } else if (!guess || guess < 1 || guess > 20) {
+    message = 'Please enter a valid number'
+    score--
+  } else if (guess === num) {
+    message = 'Yayy! You got it!'
+    document.querySelector('body').style.backgroundColor = '#60b347'
+    document.querySelector('body').style.width = '30rem'
+    document.querySelector('.number').textContent = num
+    if (score > highscore)
       document.querySelector('.highscore').textContent = score
-  } else {
-    document.querySelector('.score').textContent = score - 1
-    document.querySelector('.message').textContent = message
+  } else if (guess - num === 1 || guess - num === -1) {
+    score--
+    message = 'You are very close'
+  } else if (guess < num) {
+    score--
+    message = 'Wrong! Try going higher'
+  } else if (guess > num) {
+    score--
+    message = 'Wrong! Try going lower'
   }
+
+  document.querySelector('.score').textContent = score
+  document.querySelector('.message').textContent = message
 }
 
 const handleAgain = function () {
   num = Math.floor(Math.random() * 20) + 1
   document.querySelector('.guess').value = null
   document.querySelector('.score').textContent = 20
+  document.querySelector('.message').textContent = 'Start guessing...'
+  document.querySelector('body').style.backgroundColor = '#222'
+  document.querySelector('body').style.width = '15rem'
+  document.querySelector('.number').textContent = '?'
 }
 
 document.querySelector('.check').addEventListener('click', handleCheck)
